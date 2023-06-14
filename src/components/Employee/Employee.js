292cbe95036cb2516e5c00e392ecd86bc7180953
mamun-prodhan <mamunpr9@gmail.com>
2023-06-14 10:25:48 +0600

    event handlers with parameters

--- src/components/Employee/Employee.js
+++ src/components/Employee/Employee.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Employee = (props) => {
-    // const {name, age} = employee;
+const Employee = ({employee}) => {
+    const {_id, name, age} = employee;
+
+    const employeeDetails = (_id)=>{
+        console.log('clicked Item is: ', _id, 'Name is: ', name, 'age is: ', age);
+    }
+
     return (
         <div>
-            <h2>Employee Name: {props.employee.name}</h2>
-            <p>Employee Age: {props.employee.age}</p>
+            <h2>Employee Name: {name}</h2>
+            <p>Employee Age: {age}</p>
+            <p><small>It has Id: {_id}</small></p>
+            <button onClick={()=>employeeDetails(_id, name, age)}>Employee Details</button>
         </div>
     );
 };
